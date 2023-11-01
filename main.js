d5266cbebbf1e5d632cf0a946503855d3cea2de2
nickwhit78 <148968677+nickwhit78@users.noreply.github.com>
2023-11-01 12:00:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-const name = 'Steve';
-const rank = 'Major';
-const age = 22;
+const name = 'Rugen';
+const rank = 'Private';
+const age = 32;
 if (age < 18) {
-  console.log('You are to young for the military')
-};
- if (name === 'Rugen') {
-  console.log('My name is Inigo Montoya, you killed my father, prepare to die.')
-};
- if (rank === 'Captain') {
-  console.log('Aye Aye Captain' + name + '.')
-};
- if (rank === 'Private' && age === 40) {
-  console.log('What have you been doing with your life' + name + '?')
+  console.log('You are to young for the military');
+} else if (name === 'Rugen'&& rank === 'Count') {
+  console.log('My name is Inigo Montoya, you killed my father, prepare to die.');
+}
+ else if (rank === 'Captain') {
+  console.log('Aye Aye Captain ' + name + '.');
+}
+ else if (rank === 'Private' && age === 40) {
+  console.log('What have you been doing with your life ' + name + '?');
 }
 else(console.log('Hello Major Steve, I cannot wait to celebrate your ' + (age + 1) +'rd birthday next year!'));
